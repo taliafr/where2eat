a718711068ece54c86dcea5663122465ef2783fa
RaiyanZ <raiyanzannat02@gmail.com>
2021-09-17 22:43:14 -0500

    update

---
+++ js/Script1.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+
+class Restaurant {
+    constructor(Array foodType, rating, address, totalReviews, quote) {
+        this.rating = rating;
+        this.foodType = foodType;
+        this.address = address;
+        this.totalReviews = totalReviews;
+        this.quote = quote;
+        
+
+
+    }
+
+}

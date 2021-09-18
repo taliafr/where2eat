4b32f225e6c85841e4a99adc57055a5ff1bc6dda
RaiyanZ <raiyanzannat02@gmail.com>
2021-09-18 11:57:44 -0500

    update

---
+++ js/Restaurant.js
@@ -0,0 +1,17 @@
+// JavaScript source code
+
+class Restaurant {
+    constructor(diets, rating, address, totalReviews, quote, ethnicity, hours)
+    {
+        this.diets = diets;
+        this.rating = rating;
+        this.foodType = foodType;
+        this.address = address;
+        this.totalReviews = totalReviews;
+        this.quote = quote;
+        this.ethnicity = ethnicity;
+        this.hours = hours;
+    }
+
+
+}
